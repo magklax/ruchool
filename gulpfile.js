ee8c148e2ea83b878060b3bff89eb095d03dc432
Dmitrey Smirnov <frontend-manul@yandex.ru>
2018-10-30 10:11:15 +0300

    edits

--- gulpfile.js
+++ gulpfile.js
@@ -67,7 +67,7 @@ gulp.task('browser-sync', function() {
     browserSync.init({
         server: {
             baseDir: "./build",
-            index: "/rent.html"
+            index: "/massmedia.html"
 
 
         }
