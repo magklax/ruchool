efe481d65abce8a7640b0d516151340b5514bae2
Dmitrey Smirnov <frontend-manul@yandex.ru>
2018-11-16 15:48:16 +0300

    workshop

--- gulpfile.js
+++ gulpfile.js
@@ -67,7 +67,7 @@ gulp.task('browser-sync', function() {
     browserSync.init({
         server: {
             baseDir: "./build",
-            index: "/reviews.html"
+            index: "/workshop.html"
 
 
         }
